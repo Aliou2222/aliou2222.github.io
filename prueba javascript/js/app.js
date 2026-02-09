f844332bafb9d725299cd754ad2c42d8ef11f113
github <alisen2@alu.edu.gva.es.com>
2026-02-09 11:53:51 +0100

    probando javascript

---
+++ prueba javascript/js/app.js
@@ -0,0 +1,14 @@
+console.log("Hola mundo")
+console.log("Adios")
+console.warn("Ojo!")
+console.error("Lo has roto!")
+
+
+let nombre= "Basilisco"
+let curso= "ASIR"
+
+console.log("Te llamas: "+ nombre+ " y cursas: "+curso)
+nombre="Julieta"
+curso="DAW"
+
+console.log("Te llamas: "+ nombre+ " y cursas: "+curso)
