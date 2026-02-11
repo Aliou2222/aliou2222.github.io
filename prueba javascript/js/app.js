9e72518d05040c8a499a476d2b356b7054acb5ef
github <alisen2@alu.edu.gva.es.com>
2026-02-11 11:51:10 +0100

    probando bucles

--- prueba javascript/js/app.js
+++ prueba javascript/js/app.js
@@ -1,14 +1,42 @@
-console.log("Hola mundo")
-console.log("Adios")
-console.warn("Ojo!")
-console.error("Lo has roto!")
+// let nombre= "Basilisco"
+// let curso= "ASIR"
+// let edad=13
+
+// edad+=10
+// edad=edad+10 
+// edad=++ (lo mismo)
+
+// if (edad > 17) {
+//     console.log("puede beber")
+//     console.log("puede ir a la discoteca")
+// }
+// else{ 
+//     if (edad > 12) {
+//     console.log("puede tomar cafe")
+//     }
+//     else console.log("no puede toomar cafe")
+// }
 
 
-let nombre= "Basilisco"
-let curso= "ASIR"
+// // cadena=""
+// cadena = edad > 17 ? "puede beber" : "no puede beber";
+// console.log(cadena)
+// console.log(cadena)
+// (esta manera es mas corta)
 
-console.log("Te llamas: "+ nombre+ " y cursas: "+curso)
-nombre="Julieta"
-curso="DAW"
+// console.log("1")
+// console.log("2")
+// console.log("3")
+// console.log("4")
 
-console.log("Te llamas: "+ nombre+ " y cursas: "+curso)
+// bucle
+// let num=0
+// while (num<=10) {
+//     console.log(num)
+//     num++
+// }
+
+// let num=1
+for(num=0;num<10;num++){
+    console.log(num)
+}
